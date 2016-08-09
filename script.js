1cbc07d37a35615aa3468bd938c2023d329b9df5
Alyssa Doerscher <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-08-08 21:39:04 -0500

    updated script, added images

--- script.js
+++ script.js
@@ -6,61 +6,135 @@ $(document).ready(function () {
 	});
 
 	$('#menu1').click(function() {
-		$('#designGallery').show()
+		$('#designGallery').show();
+		$('#tracking1').show()
 	});
 	$('#menu2').click(function() {
-		$('#studioGallery').show()
+		$('#studioGallery').show();
+		$('#tracking2').show()
 	});
 
-	$('#x').click(function () {
+	$('.x').click(function () {
 		$('#designGallery, #studioGallery').hide()
 	});
 
-	$('#arrowRight').click(function () {
+	// $.fn.gallery = function(activeClass, elementClass) {
 
-		var actProj = $('.activeProj');
-		var nextProj = actProj.next('.project');
+	// 	var active = $(activeClass);
+	// 	var next = active.next();
+
+	// 	if (next.length == 0) {
+	// 		next = $(elementClass);
+	// 	}
+
+	// 	active.fadeOut(0).removeClass(activeClass);
+	// 	next.fadeIn(0).addClass(activeClass);
+	// }
+
+	// $('.arrowRight').on('click', function() {
+	// 	$.fn.gallery('.activeProj', '.project')
+	// });
+
+	$('.arrowRightD').click(function () {
+
+		var actProj = $('.activeProjD');
+		var nextProj = actProj.next('.projectD');
 
 		if (nextProj.length == 0) {
-			nextProj = $('.project').first();
+			nextProj = $('.projectD').first();
 		}
 
-		actProj.fadeOut(0).removeClass('activeProj');
-		nextProj.fadeIn(0).addClass('activeProj');
+		actProj.fadeOut(0).removeClass('activeProjD');
+		nextProj.fadeIn(0).addClass('activeProjD');
 
-		var actDot = $('.activeDot');
+		var actDot = $('.activeDotD');
 		var nextDot = actDot.next();
 
 		if (nextDot.length == 0) {
-			nextDot = $('.dot').first();
+			nextDot = $('.dotD').first();
 		}
 
-		actDot.removeClass('activeDot');
-		nextDot.addClass('activeDot');
+		actDot.removeClass('activeDotD');
+		nextDot.addClass('activeDotD');
 
 	})
 
-	$('#arrowLeft').click(function() {
 
-		var actProj = $('.activeProj');
-		var prevProj = actProj.prev('.project');
+	$('.arrowLeftD').click(function() {
+
+		var actProj = $('.activeProjD');
+		var prevProj = actProj.prev('.projectD');
 
 		if (prevProj.length == 0) {
-			prevProj = $('.project').last();
+			prevProj = $('.projectD').last();
 		}
 
-		actProj.fadeOut(0).removeClass('activeProj');
-		prevProj.fadeIn(0).addClass('activeProj');
+		actProj.fadeOut(0).removeClass('activeProjD');
+		prevProj.fadeIn(0).addClass('activeProjD');
 
-		var actDot = $('.activeDot');
+		var actDot = $('.activeDotD');
 		var	prevDot = actDot.prev();
 
 		if (prevDot.length == 0) {
-			prevDot = $('.dot').last();
+			prevDot = $('.dotD').last();
 		}
 
-		actDot.removeClass('activeDot');
-		prevDot.addClass('activeDot');
+		actDot.removeClass('activeDotD');
+		prevDot.addClass('activeDotD');
 
 	})
+
+		$('.arrowRightS').click(function () {
+
+		var actProj = $('.activeProjS');
+		var nextProj = actProj.next('.projectS');
+
+		if (nextProj.length == 0) {
+			nextProj = $('.projectS').first();
+		}
+
+		actProj.fadeOut(0).removeClass('activeProjS');
+		nextProj.fadeIn(0).addClass('activeProjS');
+
+		var actDot = $('.activeDotS');
+		var nextDot = actDot.next();
+
+		if (nextDot.length == 0) {
+			nextDot = $('.dotS').first();
+		}
+
+		actDot.removeClass('activeDotS');
+		nextDot.addClass('activeDotS');
+
+	})
+
+
+	$('.arrowLeftS').click(function() {
+
+		var actProj = $('.activeProjS');
+		var prevProj = actProj.prev('.projectS');
+
+		if (prevProj.length == 0) {
+			prevProj = $('.projectS').last();
+		}
+
+		actProj.fadeOut(0).removeClass('activeProjS');
+		prevProj.fadeIn(0).addClass('activeProjS');
+
+		var actDot = $('.activeDotS');
+		var	prevDot = actDot.prev();
+
+		if (prevDot.length == 0) {
+			prevDot = $('.dotS').last();
+		}
+
+		actDot.removeClass('activeDotS');
+		prevDot.addClass('activeDotS');
+
+	})
+
+	$('#design, #studio, #about').click(function() {
+		event.preventDefault();
+	})
+
 });
